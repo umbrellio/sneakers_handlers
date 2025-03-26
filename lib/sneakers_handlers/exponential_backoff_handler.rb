b16005133ab919a1226cedf1223a42eb9e37b8a9
Yuri Smirnov <tycoooon@gmail.com>
2025-03-26 14:02:46 +0300

    rm x-expires

--- lib/sneakers_handlers/exponential_backoff_handler.rb
+++ lib/sneakers_handlers/exponential_backoff_handler.rb
@@ -169,7 +169,6 @@ module SneakersHandlers
         :"x-dead-letter-exchange" => options[:exchange],
         :"x-dead-letter-routing-key" => queue.name,
         :"x-message-ttl" => delay * 1_000,
-        :"x-expires" => delay * 1_000 * 2
       )
     end
 
